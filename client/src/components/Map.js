f55c16035bc07d374f776b20c878c0fd900c3da5
axe-z <info@axe-z.com>
2019-04-30 12:25:59 -0400

    geolocalisation debut

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import ReactMapGL, { NavigationControl } from "react-map-gl"; //NavigationControl donne les control de zoom
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 45.50884,
+  // latitude: 37.7577,
+
+  longitude: -73.58781,
+  // longitude: -122.4376,
+
+  zoom: 13
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        mapboxApiAccessToken="pk.eyJ1IjoiYXhlLXoiLCJhIjoiY2p2MTBocm9iMXBlZzN5cXhpNWVpbzZocyJ9.C4X44au9-srE13oqcUrQvw"
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        onViewportChange={viewport => setViewport(viewport)} //permet de deplacer
+        {...viewport}>
+        <div className={classes.navigationControl}>
+          <NavigationControl onViewportChange={viewport => setViewport(viewport)} />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
